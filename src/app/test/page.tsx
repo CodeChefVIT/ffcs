e647bbd58f206454737595af8888f14f0bb70b9a
edum0 <etg9415@gmail.com>
2025-06-04 23:19:55 +0530

    created a page test to test out popups

---
+++ src/app/test/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export default function Home() {
+    return (
+        <div>
+            
+            <button className="text-white bg-blue-700 hover:bg-blue-900 focus:outilne-none text-sm rounded-lg px-5 py-2.5 text-centermr-5">
+                Gululu
+            </button>
+
+             <button className="text-white bg-blue-700 hover:bg-blue-900 focus:outilne-none text-sm rounded-lg px-5 py-2.5 text-centermr-5">
+                remove
+            </button>
+
+             <button className="text-white bg-blue-700 hover:bg-blue-900 focus:outilne-none text-sm rounded-lg px-5 py-2.5 text-centermr-5">
+                main report
+            </button>
+        </div>
+    )
+}
