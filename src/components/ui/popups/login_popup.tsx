9a3371889ab52b64efbaa04db86bd77cf6f2746e
edum0 <etg9415@gmail.com>
2025-06-09 13:24:28 +0530

    last commit

--- src/components/ui/popups/login_popup.tsx
+++ src/components/ui/popups/login_popup.tsx
@@ -8,11 +8,7 @@ interface GoogleLoginPopupProps {
 const GoogleLoginPopup: React.FC<GoogleLoginPopupProps> = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/25 backdrop-blur-sm z-50 px-4">
-<<<<<<< HEAD
       <div className="relative w-full max-w-md sm:max-w-xl md:max-w-xl bg-lime-50 rounded-3xl shadow-[7px_7px_7px_rgba(0,0,0,1.00)] outline outline-4 outline-offset-[-2px] outline-black">
-=======
-      <div className="w-full max-w-md sm:max-w-xl md:max-w-xl bg-lime-50 rounded-3xl shadow-[7px_7px_7px_rgba(0,0,0,1.00)] outline-4 outline-offset-[-2px] outline-black">
->>>>>>> 2b0ead19b6e91bf11071fe6d18f3ff1c4ca17f17
         {/* Header */}
         <div className="flex justify-between items-center h-14 bg-amber-400 rounded-t-3xl outline-4 outline-offset-[-2px] outline-black px-4">
           <div className="flex items-center gap-2">
@@ -44,23 +40,12 @@ const GoogleLoginPopup: React.FC<GoogleLoginPopupProps> = ({ onClose }) => {
         </div>
 
         {/* Google Login Button */}
-<<<<<<< HEAD
         <div className="flex justify-center mt-2 px-18">
           <button className="flex items-center justify-center gap-4 w-full max-w-80 h-14 bg-white rounded-3xl shadow-[5px_5px_0px_rgba(0,0,0,1.00)] outline outline-4 outline-black px-4">
             <img
               src="./google.svg"
               alt="Google"
               className="w-10 h-10"
-=======
-        <div className="flex justify-center mt-6 px-4">
-          <button className="flex items-center justify-center gap-4 w-full max-w-sm h-14 bg-white rounded-3xl shadow-[5px_5px_0px_rgba(0,0,0,1.00)] outline-4 outline-black px-4">
-            <Image
-              src="./google.svg"
-              alt="Google"
-              width={120}
-              height={80}
-              className="w-6 h-6"
->>>>>>> 2b0ead19b6e91bf11071fe6d18f3ff1c4ca17f17
             />
             <span className="text-black text-lg sm:text-xl md:text-lg font-bold font-['Poppins']">
               Login with Google
