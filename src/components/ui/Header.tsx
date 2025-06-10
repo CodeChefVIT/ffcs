8632e7034ad883bab586815bf62add14c22e2975
Ishan Jindal <jindal25ishan@gmail.com>
2025-06-10 18:17:44 +0530

    ..

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Header() {
     return (
@@ -10,19 +11,80 @@ export default function Header() {
                 className="absolute top-0 left-1/2 -translate-x-1/2 w-full max-w-none h-auto z-0 object-cover"
             />
 
-            <img src="/paper.svg" alt="paper" className="absolute left-[30%] top-[25%] sm:left-[5%] sm:top-[30%] md:left-[10%] md:top-[40%] lg:left-[7%] w-24 lg:w-32 z-10" />
-            <img src="/plane.svg" alt="plane" className="absolute right-[10%] top-[30%] w-28 sm:right-[5%] sm:top-[40%] md:top-[50%] lg:w-36 z-10 lg:right-[10%]" />
-            <img src="/c.svg" alt="C" className="absolute left-[15%] top-[70%] sm:left-[10%] sm:top-[87%] w-8 z-10 lg:w-11" />
-            <img src="/h.svg" alt="H" className="absolute left-[25%] top-[22%] w-8 z-10 md:top-[30%] lg:w-11" />
-            <img src="/e.svg" alt="E" className="absolute right-[20%] top-[70%] sm:top-[80%] w-8 z-10 lg:w-11" />
-            <img src="/f.svg" alt="F" className="absolute right-[10%] top-[78%] w-8 sm:top-[88%] z-10 lg:w-11" />
+            <Image
+                src="/paper.svg"
+                alt="paper"
+                width={96} // = w-24
+                height={96} // ~ h-24
+                className="absolute left-[30%] top-[25%] sm:left-[5%] sm:top-[30%] md:left-[10%] md:top-[40%] lg:left-[7%] lg:w-32 z-10"
+            />
 
-            <img src="/scribble-left.svg" alt="scribble" className="absolute left-[25%] top-[75%] sm:left-[13%] sm:top-[73%] md:left-[20%] w-14 z-10 lg:w-16 lg:top-[78%]" />
-            <img src="/scribble-right.svg" alt="squiggle" className="absolute right-[25%] top-[25%] sm:right-[20%] md:top-[35%] w-14 z-10 lg:w-16 lg:top-[30%] lg:right-[25%]" />
+
+            <Image
+                src="/plane.svg"
+                alt="plane"
+                width={112} // w-28
+                height={112}
+                className="absolute right-[10%] top-[30%] sm:right-[5%] sm:top-[40%] md:top-[50%] lg:w-36 z-10 lg:right-[10%]"
+            />
+
+            <Image
+                src="/c.svg"
+                alt="C"
+                width={32} // w-8
+                height={32}
+                className="absolute left-[15%] top-[70%] sm:left-[10%] sm:top-[87%] z-10 lg:w-11"
+            />
+
+            <Image
+                src="/h.svg"
+                alt="H"
+                width={32} // w-8
+                height={32}
+                className="absolute left-[25%] top-[22%] z-10 md:top-[30%] lg:w-11"
+            />
+
+            <Image
+                src="/e.svg"
+                alt="E"
+                width={32} // w-8
+                height={32}
+                className="absolute right-[20%] top-[70%] sm:top-[80%] z-10 lg:w-11"
+            />
+
+            <Image
+                src="/f.svg"
+                alt="F"
+                width={32} // w-8
+                height={32}
+                className="absolute right-[10%] top-[78%] sm:top-[88%] z-10 lg:w-11"
+            />
+
+            <Image
+                src="/scribble-left.svg"
+                alt="scribble"
+                width={56} // w-14
+                height={56}
+                className="absolute left-[25%] top-[75%] sm:left-[13%] sm:top-[73%] md:left-[20%] z-10 lg:w-16 lg:top-[78%]"
+            />
+
+            <Image
+                src="/scribble-right.svg"
+                alt="squiggle"
+                width={56} // w-14
+                height={56}
+                className="absolute right-[25%] top-[25%] sm:right-[20%] md:top-[35%] z-10 lg:w-16 lg:top-[30%] lg:right-[25%]"
+            />
 
             <div className="z-20 pt-40 pb-20">
                 <div className="flex flex-col items-center justify-start gap-5">
-                    <img src="/ffcs.svg" alt="icon" className="w-24 h-24 mb-4" />
+                    <Image
+                        src="/ffcs.svg"
+                        alt="icon"
+                        width={94} // w-24 = 96px
+                        height={94} // h-24 = 96px
+                        className="mb-4"
+                    />
 
                     <h1 className="text-4xl lg:text-5xl font-[400] mb-2 tracking-wide font-[Pangolin]">FFCS-inator</h1>
 
@@ -35,9 +97,15 @@ export default function Header() {
                         Create Your Ideal Timetable!
                     </h2>
 
-                    <button className="font-poppins font-normal bg-[#A9CFFF] px-6 py-1 rounded-xl border-[3px] border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 flex items-center gap-5 text-2xl">
+                    <button className="font-poppins font-normal bg-[#A9CFFF] px-6 py-1 rounded-xl border-[3px] border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all duration-200 font-normal flex items-center gap-5 text-2xl">
                         Start
-                        <img src="/arrow-down.svg" alt="down" className="w-7 h-7" />
+
+                        <Image
+                            src="/arrow-down.svg"
+                            alt="down"
+                            width={28} // w-7 = 28px
+                            height={28} // h-7 = 28px
+                        />
                     </button>
                 </div>
             </div>
@@ -52,7 +120,12 @@ export default function Header() {
             </div>
 
             <div className="font-poppins font-normal absolute top-6 left-6 flex items-center gap-4 z-30">
-                <img src="/cc-border.svg" alt="Logo" className="w-12 h-12" />
+                <Image
+                    src="/cc-border.svg"
+                    alt="Logo"
+                    width={48} // w-12 = 48px
+                    height={48} // h-12 = 48px
+                />
                 <button className="bg-[#FFEA79] cursor-pointer px-6 py-2 rounded-xl border-[3px] border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] font-semibold text-sm sm:text-base">
                     Slot View
                 </button>
