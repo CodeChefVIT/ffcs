62f322929e9fbd4e8481a00fad289ee643cd2773
Atharva Sharma <atharva17asm@gmail.com>
2025-06-16 16:07:36 +0530

    Add : ShareID generator

--- src/app/api/save-timetable/route.ts
+++ src/app/api/save-timetable/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/db";
 import Timetable from "@/models/timetable";
+import { generateShareId } from "@/lib/shareIDgenerate";
 
 export async function POST(req: NextRequest) {
   await dbConnect();
@@ -13,11 +14,19 @@ export async function POST(req: NextRequest) {
   }
 
   try {
+    let shareId: string;
+    let exists = true;
+    do {
+      shareId = generateShareId();
+      exists = !!(await Timetable.exists({ shareId }));
+    } while (exists);
+
     const timetable = await Timetable.create({
       title,
       slots,
       owner,
       isPublic: false,
+      shareId,
     });
     return NextResponse.json({ success: true, timetable });
   } catch (error) {
