6ad6e0523995c6cd624460f7696d5be338c8d691
Atharva Sharma <atharva17asm@gmail.com>
2025-06-17 17:48:47 +0530

    login check on saved page

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -1,14 +1,47 @@
-"use client"
+"use client";
 
-import useScreenSize from '@/hooks/useScreenSize'
-import Saved from './saved'
-import SavedMobile from './saved-mobile'
-import Loader from '@/components/ui/Loader'
+import { useEffect, useState } from 'react';
+import useScreenSize from '@/hooks/useScreenSize';
+import Saved from './saved';
+import SavedMobile from './saved-mobile';
+import Loader from '@/components/ui/Loader';
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import AlertModal from '@/components/ui/AlertModal'; 
 
 export default function View() {
-  const size = useScreenSize()
+  const { status } = useSession(); 
+  const router = useRouter();
+  const size = useScreenSize();
 
-  if (size === null) return <Loader />;
-  if (size === 'mobile') return <SavedMobile />
-  return <Saved />
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      setShowAlert(true);
+    }
+  }, [status]);
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+    router.push('/');
+  };
+
+  if (status === 'loading' || size === null) return <Loader />;
+
+  if (status !== 'authenticated') {
+    return (
+      <>
+        {showAlert && (
+          <AlertModal
+            open={showAlert}
+            message="Please login to continue."
+            onClose={handleCloseAlert}
+          />
+        )}
+      </>
+    );
+  }
+
+  return size === 'mobile' ? <SavedMobile /> : <Saved />;
 }
