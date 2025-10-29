15eeea30993c2296ee8b7ba25f005a9ad2cd400d
Samya <mehtasamya10@gmail.com>
2025-10-29 21:23:42 +0530

    feat:sbst&scheme done

--- src/data/SBST/DisciplineLinked.ts
+++ src/data/SBST/DisciplineLinked.ts
@@ -1,31 +1,20 @@
 export const disciplineLinked = {
   'BBIT201L - Principles of Chemical Engineering': [
-    { slot: 'F1+TF1', venue: 'SMV116', faculty: 'DHANYA V' },
-    { slot: 'F2+TF2', venue: 'SMV116', faculty: 'DHANYA V' },
-    { slot: 'B1+TB1', venue: 'SMV121', faculty: 'SREEJA S' },
-    { slot: 'B2+TB2', venue: 'SMV121', faculty: 'THIRUPATHI KUMARA RAJA S' },
+    { slot: 'C1+TC1', venue: 'SMV121', faculty: 'DHANYA V' },
   ],
   'BBIT201P - Chemical Engineering Lab': [
-    { slot: 'L1+L2', venue: 'SMVG26', faculty: 'THIRUPATHI KUMARA RAJA S' },
-    { slot: 'L5+L6', venue: 'SMVG26', faculty: 'DHANYA V' },
-    { slot: 'L37+L38', venue: 'SMVG26', faculty: 'DHANYA V' },
-    { slot: 'L31+L32', venue: 'SMVG26', faculty: 'SREEJA S' },
-    { slot: 'L43+L44', venue: 'SMVG26', faculty: 'DHANYA V' },
-    { slot: 'L9+L10', venue: 'SMVG26', faculty: 'DHANYA V' },
-    { slot: 'L55+L56', venue: 'SMVG26', faculty: 'SREEJA S' },
-    { slot: 'L15+L16', venue: 'SMVG26', faculty: 'THIRUPATHI KUMARA RAJA S' },
+    { slot: 'L45+L46', venue: 'SMVG26', faculty: 'DHANYA V' },
   ],
-  'BBIT301L - Principles of Bioprocess Engineering': [
-    { slot: 'E1+TE1', venue: 'SMV116', faculty: 'SANGEETHA SUBRAMANIAN' },
-    { slot: 'E1+TE1', venue: 'SMV121', faculty: 'ROHIT' },
-    { slot: 'E2+TE2', venue: 'SMV116', faculty: 'RAMESH PATHY M' },
+  'BBIT205L - Bioinformatics': [
+    { slot: 'F1', venue: 'SMV124', faculty: 'SHANTHI V' },
+    { slot: 'F2', venue: 'SMV121', faculty: 'SUDANDIRA DOSS C' },
+    { slot: 'F2', venue: 'SMV124', faculty: 'RAJINIRAJA M' },
+    { slot: 'F1', venue: 'SMV121', faculty: 'RAMANATHAN K' },
   ],
-  'BBIT301P - Bioprocess Engineering Lab': [
-    { slot: 'L31+L32', venue: 'SMV225', faculty: 'ROHIT' },
-    { slot: 'L37+L38', venue: 'SMV225', faculty: 'PRIYANKAR SEN' },
-    { slot: 'L23+L24', venue: 'SMV225', faculty: 'KALI KISHORE REDDY TETALA' },
-    { slot: 'L31+L32', venue: 'SMV225', faculty: 'SANGEETHA SUBRAMANIAN' },
-    { slot: 'L23+L24', venue: 'SMV225', faculty: 'RAMESH PATHY M' },
-    { slot: 'L37+L38', venue: 'SMV225', faculty: 'ROHIT' },
+  'BBIT205P - Bioinformatics Lab': [
+    { slot: 'L37+L38', venue: 'SMVG17', faculty: 'SHANTHI V' },
+    { slot: 'L5+L6', venue: 'SMVG15', faculty: 'SUDANDIRA DOSS C' },
+    { slot: 'L27+L28', venue: 'SMVG15', faculty: 'RAJINIRAJA M' },
+    { slot: 'L49+L50', venue: 'SMVG15', faculty: 'RAMANATHAN K' },
   ],
 };
--- src/data/SCHEME/DisciplineElective.ts
+++ src/data/SCHEME/DisciplineElective.ts
@@ -1,26 +1,39 @@
 export const SchemeDisciplineElective = {
   'BCHE310L - Polymer Technology': [
-    { slot: 'F1+TF1', venue: 'SMV102', faculty: 'PANDURANGAN.K' },
-    { slot: 'D1+TD1', venue: 'SMV109', faculty: 'MOHAMMED REHAAN CHANDAN' },
+    { slot: 'F1+TF1', venue: 'SMV109', faculty: 'AABID HUSSAIN SHAIK' },
   ],
-  'BCHE312L - Chemical Process Optimization': [
-    { slot: 'G1+TG1', venue: 'SMV109', faculty: 'DHARMENDRA KUMAR BAL' },
+  'BCHE314L - Fuels and Combustion': [
+    { slot: 'F1+TF1', venue: 'SMV104', faculty: 'MOHAMMED REHAAN CHANDAN' },
   ],
-  'BCHE313L - Environmental Pollution Control': [
-    { slot: 'F1+TF1', venue: 'SMV104', faculty: 'SHISHIR KUMAR BEHERA' },
-  ],
-  'BCHE314L - Fuels and Combustion': [{ slot: 'F2+TF2', venue: 'SMV102', faculty: 'CHITRA  D' }],
   'BCHE315L - Biochemical Engineering': [
-    { slot: 'F1+TF1', venue: 'SMV109', faculty: 'THOMAS THEODORE' },
+    { slot: 'F2+TF2', venue: 'SMV102', faculty: 'RAMKUMAR N' },
+    { slot: 'F1+TF1', venue: 'SMVG21', faculty: 'GANESH MOORTHY I' },
+  ],
+  'BCHE316L - Pharmaceutical Technology': [
+    { slot: 'E2+TE2', venue: 'SMV109', faculty: 'DHARMENDRA KUMAR BAL' },
   ],
   'BCHE317L - Petroleum Refining Technology': [
-    { slot: 'E1+TE1', venue: 'SMV101', faculty: 'SIVAGAMI K' },
+    { slot: 'F1+TF1', venue: 'SMV102', faculty: 'ASLAM ABDULLAH M' },
+    { slot: 'F2+TF2', venue: 'SMV109', faculty: 'PRABAKAR P' },
   ],
   'BCHE318L - Safety and Hazard Analysis': [
-    { slot: 'F2+TF2', venue: 'SMV109', faculty: 'BABU PONNUSAMI A' },
+    { slot: 'F2+TF2', venue: 'SMV104', faculty: 'MAHESH GANESAPILLAI' },
   ],
   'BCHE319E - Process Plant Design and Simulation': [
-    { slot: 'G2', venue: 'SMV109', faculty: 'MURUGANANDAM L' },
+    { slot: 'G2', venue: 'SMV104', faculty: 'MURUGANANDAM L' },
     { slot: 'L3+L4', venue: 'SMVG17', faculty: 'MURUGANANDAM L' },
+  ],
+  'BCHE320L - Chemical Product Design': [
+    { slot: 'E1+TE1', venue: 'SMV101', faculty: 'SHANKAR RAMAN DHANUSHKODI' },
+  ],
+  'BCHE322L - Nanoscience and Nanotechnology': [
+    { slot: 'G1+TG1', venue: 'SMVG21', faculty: 'MADAMBAKAM MIDHUN KUMAR' },
+  ],
+  'BCHE402L - Food Process Engineering': [
+    { slot: 'F2+TF2', venue: 'SMVG21', faculty: 'SHISHIR KUMAR BEHERA' },
+    { slot: 'F1+TF1', venue: 'SMVG10', faculty: 'ARUNA SINGH' },
+  ],
+  'BCHE406L - AI in Chemical Engineering': [
+    { slot: 'G2+TG2', venue: 'SMVG21', faculty: 'BANDARU KIRAN' },
   ],
 };
